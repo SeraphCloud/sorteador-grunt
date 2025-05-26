56f254d74379249f9b0af7e469e54eb548c3ad44
Victor <alexandre.victor2641@gmail.com>
2025-05-26 17:57:42 -0300

    dist bug fix

--- gruntfile.js
+++ gruntfile.js
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
                         },
                         {
                             match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
